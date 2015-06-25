4b5f43dfb93824bcd20b604e2b558f8691bf64d2
Dylan Nguyen <dyng9409@colorado.edu>
2015-06-25 15:51:47 -0600

    finished creating and modifying auto test

---
+++ testing/webTest.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'Testing Site': function (test) {
+        test
+            .open('http://localhost:8000/letsfinda.html')
+            .assert.title().is('Letsfinda ...', 'It has title')
+            .type('#autocomplete', 'Denver, CO, United States')
+            .assert.val('#autocomplete', 'Denver, CO, United States', 'Text Field AcceptS Input')
+            .click('#dateinput')
+            .assert.val('#dateinput', '06-25-2015', 'Date Field Populates')
+            .type('#money', '10.00')
+            .assert.val('#money', '10.00', 'Price Field Populates')
+            .sendKeys('#money','\uE013')
+            .assert.val('#money', '10.01', 'Increment price works')
+            .sendKeys('#money','\uE015')
+            .assert.val('#money', '10', 'Decrement price works')
+            .click('#flat')
+            .accept()
+            .done();
+
+    }
+   /* 
+    'Autocompletion': function (test) {
+        test
+            .open('http://localhost:8000/letsfinda.html')
+            .type('#autocomplete', 'den')
+            .sendKeys('body','\u001B\u005B\u0042')
+            .sendKeys('body','\uE006')
+            .assert.val('#autocomplete', 'Denver', 'Autocomplete Worked')
+            .done();
+    }
+   */ 
+};
+
